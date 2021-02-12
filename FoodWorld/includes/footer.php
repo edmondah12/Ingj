645ed22ab2023b392f49e0443913d50f06f3ad59
edmondah12 <78389695+edmondah12@users.noreply.github.com>
2021-02-12 23:05:04 +0100

    Delete FoodWorld directory

--- FoodWorld/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="Foot">
-    <br>
-    <p>Food recipes from all around the world!</p>
-    <p>Thank you for visiting us!</p>
-    <br>
-    <p> FoodWorld.com &copy;Copyright 2020 </p>
-</footer>
